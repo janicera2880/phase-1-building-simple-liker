946e8a25708dcbcfa9fa36178e58ec4bc06e47df
Janice Alecha <janiceralecha@gmail.com>
2022-06-09 19:08:25 -0500

    commit tree 5f2442ea56be4d6ce3a8552e429381476402d1d9

--- main.js
+++ main.js
@@ -2,10 +2,37 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const theHearts = document.querySelectorAll('.like-glyph')
+
+function likeHeart(e) {
+  const heart = e.target;
+console.log(heart)
+  mimicServerCall()
+    .then(function(){
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART
+        heart.className = "activated-heart"
+      } else {
+        heart.innerText = EMPTY_HEART
+        heart.className = ""
+      }
+    })
+    .catch(function (error) {
+      const modal = document.getElementById('modal')
+      modal.className = ""
+      modal.innerText = error
+
+      setTimeout(function() {
+          modal.className = "hidden"
+        },3000)
+      }
+)
+}
 
 
-
+for (const heart of theHearts) {
+  heart.addEventListener("click", likeHeart);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -20,6 +47,6 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       } else {
         resolve("Pretend remote server notified of action!");
       }
-    }, 300);
+    }, 3000);
   });
 }
